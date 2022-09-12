df6a2259061264572fc52b34071c526eef9bd8d2
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-09-12 18:19:25 +0200

    Finish sign up

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,18 +1,34 @@
 import React from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 
-// interface LogInProps {
-
-// }
 
 type FormValues = {
   email: string;
   password: string;
 };
 
+const logInAuth = (user: FormValues) => {
+
+    const stringData = localStorage.getItem('users')
+    const users: FormValues[] = (stringData) ? JSON.parse(stringData) : [];
+    console.log(users);
+    users.forEach((data)=> {
+          if((data.email === user.email) && (data.password === user.password)) {
+              alert('Signned in successfully')
+              localStorage.setItem('currentUser', JSON.stringify(data))
+              console.log('sucess')
+          } else {
+              console.log('invalid')
+          }
+    });
+}
+
 const LogIn: React.FC = () => {
   const { register, handleSubmit } = useForm<FormValues>();
-  const onSubmit: SubmitHandler<FormValues> = (data) => console.log(data);
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    // console.log(data)
+    logInAuth(data)
+  };
   return (
     <>
       <h2>Log in Form</h2>
