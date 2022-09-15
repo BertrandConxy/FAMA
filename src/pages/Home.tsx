01327d007dd404291ec4dd190b7b8b38ecb79ee1
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-09-15 16:37:52 +0200

    Add the auth context

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, {useContext} from "react";
+import { AuthContext } from "../App";
 
 const Home: React.FC = () => {
-  return <h1>Hello</h1>;
+  const {auth, toggleAuth} = useContext(AuthContext);
+  return (
+    <>
+    <h1>
+      Hello <span>{`${auth}`}</span>{" "}
+    </h1>
+    <button  onClick={
+      () => toggleAuth()
+    } >Click</button>
+    </>
+  );
 };
 
 export default Home;
